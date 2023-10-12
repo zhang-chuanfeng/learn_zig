355c5bdf40d4c35b8fc70ee199be991cb900fdd7
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2023-10-12 14:28:06 +0800

    [MOD]test memory leak

---
+++ test_memory_leak.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+test "detect leak" {
+    var list = std.ArrayList(u21).init(std.testing.allocator);
+    // missing
+    // defer list.deinit();
+    try list.append('☔');
+
+    try std.testing.expect(list.items.len == 1);
+}
