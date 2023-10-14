aa53b0d407ea9b234ae7c7121f5fbb1d82523d00
zhang-chuanfeng <654300242@qq.com>
2023-10-14 15:37:56 +0800

    [ADD]test null terminated array

---
+++ test_null-terminates_array.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "null terminated array" {
+    const array = [_:0]u8{ 1, 2, 3, 4 };
+
+    try expect(@TypeOf(array) == [4:0]u8);
+    try expect(array.len == 4);
+    try expect(array[4] == 0);
+}
