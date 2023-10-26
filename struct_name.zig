3537ecddf1d11b8b73479df5db24e3e99a44e33a
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2023-10-26 17:59:02 +0800

    [ADD]struct learn

---
+++ struct_name.zig
@@ -0,0 +1,56 @@
+const std = @import("std");
+
+pub fn main() void {
+    const Foo = struct {};
+    std.debug.print("variable: {s}\n", .{@typeName(Foo)});
+    std.debug.print("anonymous: {s}\n", .{@typeName(struct {})});
+    std.debug.print("function: {s}\n", .{@typeName(List(i32))});
+}
+
+fn List(comptime T: type) type {
+    return struct { x: T };
+}
+
+// struct type can be inferred
+
+test "fully anonymous struct" {
+    try dump(.{
+        .int = 1234,
+        .float = 12.34,
+        .b = true,
+        .s = "hi",
+    });
+}
+
+const expect = @import("std").testing.expect;
+fn dump(args: anytype) !void {
+    try expect(args.int == 1234);
+    try expect(args.float == 12.34);
+    try expect(args.b);
+    try expect(args.s[0] == 'h');
+    try expect(args.s[1] == 'i');
+}
+
+// struct tuple
+// anonymous structs
+// without specifying field names
+
+test "tuple" {
+    const values = .{
+        @as(u32, 1234),
+        @as(f64, 12.34),
+        true,
+        "hi",
+    } ++ .{false} ** 2;
+
+    try expect(values[0] == 1234);
+    try expect(values[4] == false);
+
+    inline for (values, 0..) |v, i| {
+        if (i != 2) continue;
+        try expect(v);
+    }
+    try expect(values.len == 6);
+    try expect(values.@"3"[0] == 'h');
+    try expect(values[3][1] == 'i');
+}
--- test_struct.zig
+++ test_struct.zig
@@ -120,3 +120,129 @@ test "linked list" {
     try expect(list2.first.?.data == 1234);
     // instead of try expect(list2.first.?.*.data == 1234);
 }
+
+// struct default field values
+const Foo = struct { a: i32 = 1234, b: i32 };
+
+test "default struct initialization fields" {
+    const x = Foo{
+        .b = 5,
+    };
+    if (x.a + x.b != 1239) {
+        @compileError("it's even comptime-known!");
+    }
+}
+
+// extern struct
+// This kind of struct should only be used for compatibility with the C ABI.
+
+// packed struct
+// packed structs have guaranteed in-memory layout.
+const std = @import("std");
+const native_endian = @import("builtin").target.cpu.arch.endian();
+const Full = packed struct {
+    number: u16,
+};
+
+const Divided = packed struct {
+    half1: u8,
+    quarter3: u4,
+    quarter4: u4,
+};
+
+test "@bitCast between packed structs" {
+    try doTheTest();
+    try comptime doTheTest();
+}
+
+fn doTheTest() !void {
+    try expect(@sizeOf(Full) == 2);
+    try expect(@sizeOf(Divided) == 2);
+    var full = Full{ .number = 0x1234 };
+    var divided: Divided = @bitCast(full);
+    try expect(divided.half1 == 0x34);
+    try expect(divided.quarter3 == 0x2);
+    try expect(divided.quarter4 == 0x1);
+
+    var ordered: [2]u8 = @bitCast(full);
+    switch (native_endian) {
+        .Big => {
+            try expect(ordered[0] == 0x12);
+            try expect(ordered[1] == 0x34);
+        },
+        .Little => {
+            // std.debug.print("little\n", .{});
+            try expect(ordered[0] == 0x34);
+            try expect(ordered[1] == 0x12);
+        },
+    }
+}
+
+// non-byte_aligned_field
+const BitField = packed struct {
+    a: u3,
+    b: u3,
+    c: u2,
+};
+
+var foo = BitField{
+    .a = 1,
+    .b = 2,
+    .c = 3,
+};
+
+test "pointer to non-byte-aligned field" {
+    const ptr = &foo.b;
+    try expect(ptr.* == 2);
+}
+
+test "pointer to non-bit-aligned field" {
+    //try expect(bar(&foo.b) == 2);
+}
+
+// x: ABI aligned pointer
+fn bar(x: *const u3) u3 {
+    return x.*;
+}
+
+test "pointers of sub-byte-aligned fields share address" {
+    try expect(@intFromPtr(&foo.a) == @intFromPtr(&foo.b));
+    try expect(@intFromPtr(&foo.a) == @intFromPtr(&foo.c));
+
+    // bitOffsetOf
+    // offsetOf
+    try expect(@bitOffsetOf(BitField, "a") == 0);
+    try expect(@bitOffsetOf(BitField, "b") == 3);
+    try expect(@bitOffsetOf(BitField, "c") == 6);
+
+    try expect(@offsetOf(BitField, "a") == 0);
+    try expect(@offsetOf(BitField, "b") == 0);
+    try expect(@offsetOf(BitField, "c") == 0);
+}
+
+// overaligned packed struct
+const S = packed struct {
+    a: u32,
+    b: u32,
+};
+
+test "overaligned pointer to packed struce" {
+    var foo1: S align(4) = .{ .a = 1, .b = 2 };
+    const ptr: *align(4) S = &foo1;
+    const ptr_to_b: *u32 = &ptr.b;
+    try expect(ptr_to_b.* == 2);
+}
+
+const expectEqual = @import("std").testing.expectEqual;
+// aligned struct fields
+test "aligned struct fields" {
+    const S = struct {
+        a: u32 align(2),
+        b: u32 align(64),
+    };
+    var foo = S{ .a = 1, .b = 2 };
+
+    try expectEqual(64, @alignOf(S));
+    try expectEqual(*align(2) u32, @TypeOf(&foo.a));
+    try expectEqual(*align(64) u32, @TypeOf(&foo.b));
+}
