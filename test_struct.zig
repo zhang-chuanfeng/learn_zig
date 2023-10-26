2078ca9972cebae9043bf3957afbbd12ad6f1402
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2023-10-26 18:03:44 +0800

    [ADD]mod warning

--- test_struct.zig
+++ test_struct.zig
@@ -236,13 +236,13 @@ test "overaligned pointer to packed struce" {
 const expectEqual = @import("std").testing.expectEqual;
 // aligned struct fields
 test "aligned struct fields" {
-    const S = struct {
+    const SS = struct {
         a: u32 align(2),
         b: u32 align(64),
     };
-    var foo = S{ .a = 1, .b = 2 };
+    var foos = SS{ .a = 1, .b = 2 };
 
-    try expectEqual(64, @alignOf(S));
-    try expectEqual(*align(2) u32, @TypeOf(&foo.a));
-    try expectEqual(*align(64) u32, @TypeOf(&foo.b));
+    try expectEqual(64, @alignOf(SS));
+    try expectEqual(*align(2) u32, @TypeOf(&foos.a));
+    try expectEqual(*align(64) u32, @TypeOf(&foos.b));
 }
