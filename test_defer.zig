9fbc7f5cae004c94d122a43298a77c4d18c7ce88
zhang-chuanfeng <654300242@qq.com>
2023-10-31 22:02:16 +0800

    [ADD]zig test errdefer

--- test_defer.zig
+++ test_defer.zig
@@ -38,3 +38,51 @@ fn deferUnwindExample() void {
 test "defer unwinding" {
     deferUnwindExample();
 }
+
+// Inside a defer expression the return statement is not allowed
+
+fn deferInvalidExample() !void {
+    defer {
+        // return error.DeferError;
+    }
+    return error.DeferError;
+}
+
+// The errdefer keyword is similar to derfer, but will only execute if
+// the scope returns with an error
+
+// This is especially useful in allowing a function to clean up properly
+// on error, and replaces goto error handling tacting tactics as seen in c.
+
+fn deferErrorExample(is_error: bool) !void {
+    print("\nstart of function\n", .{});
+
+    // This will always be excuted on exit
+    defer {
+        print("end of function\n", .{});
+    }
+
+    errdefer {
+        print("encountered an error!\n", .{});
+    }
+
+    if (is_error) {
+        return error.DeferError;
+    }
+}
+
+// The errdefer keyword also supports an alternative syntax to capture the
+// generated error.
+// This is useful for printing an additional error message during clean up.
+fn deferErrorCaptureExample() !void {
+    errdefer |err| {
+        std.debug.print("the error is {s}\n", .{@errorName(err)});
+    }
+    return error.DeferError;
+}
+
+test "errdefer unwinding" {
+    deferErrorExample(false) catch {};
+    deferErrorExample(true) catch {};
+    deferErrorCaptureExample() catch {};
+}
