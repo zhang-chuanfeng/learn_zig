f00b386248ec4f3631c796e0387bdd6cedbef308
zhang-chuanfeng <654300242@qq.com>
2023-11-05 17:23:55 +0800

    [ADD]test type coerion peer type resolve

--- test_casting.zig
+++ test_casting.zig
@@ -126,3 +126,183 @@ test "*T to *[1]T" {
     const z: [*]i32 = y;
     try expect(z[0] == 1234);
 }
+
+// coerion optionals
+test "coerce to optionals" {
+    const x: ?i32 = 1234;
+    const y: ?i32 = null;
+
+    try expect(x.? == 1234);
+    try expect(y == null);
+}
+
+test "coerce to optionals wrapped in error union" {
+    const x: anyerror!?i32 = 1234;
+    const y: anyerror!?i32 = null;
+
+    try expect((try x).? == 1234);
+    try expect((try y) == null);
+}
+
+test "coering large interger type to smaller one value is comptime-known to fit" {
+    const x: u64 = 255;
+    const y: u8 = x;
+    try expect(y == 255);
+}
+
+const E = enum {
+    one,
+    two,
+    three,
+};
+
+const U = union(E) {
+    one: i32,
+    two: f32,
+    three,
+};
+
+const U2 = union(enum) {
+    a: void,
+    b: f32,
+
+    fn tag(self: U2) usize {
+        switch (self) {
+            .a => return 1,
+            .b => return 2,
+        }
+    }
+};
+
+test "coerion between unions and enums" {
+    var u = U{ .two = 12.34 };
+    var e: E = u; // coerce union to enum
+    try expect(e == E.two);
+
+    const three = E.three;
+    var u_2: U = three; // coerce enum to union
+    try expect(u_2 == E.three);
+
+    var u_3: U = .three;
+    try expect(u_3 == E.three);
+
+    var u_4: U2 = .a; // coerce enum literal to union with inferred enum tag type.
+    try expect(u_4.tag() == 1);
+
+    // The following example is invalid.
+    // error: coerion from enum '@TypeOf(.enum_literal)' to union 'test_coerce_unions_enum.U2' must
+    // initialize 'f32' field 'b'
+    // var u_5:U2 = .b;
+    // try expect(u_5.tag() == 2);
+}
+
+// type coercion: Tuples to arrays
+// tupels can be coerced to arrays, if all of the fields have the same type.
+const Tuple = struct { u8, u8 };
+test "coercion from homogenous tuple to array" {
+    const tuple: Tuple = .{ 5, 6 };
+    const array: [2]u8 = tuple;
+    _ = array;
+}
+
+// explicit casts
+// Builtin functions
+// @bitCast .... @ptrCast ...
+
+// Peer Type Resolution
+// occurs in these places
+// swith expression
+// if expression
+// while / for expression
+// Multipile break statements in a block
+// Some binary operations
+
+test "peer resolve int widening" {
+    var a: i8 = 12;
+    var b: i16 = 34;
+    var c = a + b;
+    try expect(c == 46);
+    try expect(@TypeOf(c) == i16);
+}
+
+test "peer resolve arrays of different size to const slice" {
+    try expect(mem.eql(u8, booToStr(true), "true"));
+    try expect(mem.eql(u8, booToStr(false), "false"));
+    try comptime expect(mem.eql(u8, booToStr(true), "true"));
+    try comptime expect(mem.eql(u8, booToStr(false), "false"));
+}
+fn booToStr(b: bool) []const u8 {
+    return if (b) "true" else "false";
+}
+
+test "peer resolve array and const slice" {
+    try testPeerResolveArrayConstSlice(true);
+    try comptime testPeerResolveArrayConstSlice(true);
+}
+fn testPeerResolveArrayConstSlice(b: bool) !void {
+    const value1 = if (b) "aoeu" else @as([]const u8, "zz");
+    const value2 = if (b) @as([]const u8, "zz") else "aoeu";
+
+    try expect(mem.eql(u8, value1, "aoeu"));
+    try expect(mem.eql(u8, value2, "zz"));
+}
+
+test "peer type resolution: ?T and T" {
+    try expect(peerTypeTAndOptionalT(true, false) == 0);
+    try expect(peerTypeTAndOptionalT(false, false) == 3);
+
+    comptime {
+        try expect(peerTypeTAndOptionalT(true, false) == 0);
+        try expect(peerTypeTAndOptionalT(false, false) == 3);
+    }
+}
+fn peerTypeTAndOptionalT(c: bool, b: bool) ?usize {
+    if (c) {
+        return if (b) null else @as(usize, 0);
+    }
+    return @as(usize, 3);
+}
+
+test "peer type resolution:*[0]u8 and []const u8" {
+    try expect(peerTypeEmptyArrayAndSlice(true, "hi").len == 0);
+    try expect(peerTypeEmptyArrayAndSlice(false, "hi").len == 1);
+    comptime {
+        try expect(peerTypeEmptyArrayAndSlice(true, "hi").len == 0);
+        try expect(peerTypeEmptyArrayAndSlice(false, "hi").len == 1);
+    }
+}
+
+fn peerTypeEmptyArrayAndSlice(a: bool, slice: []const u8) []const u8 {
+    if (a) {
+        return &[_]u8{};
+    }
+    return slice[0..1];
+}
+
+test "peer type resolution: *[0]u8, []const u8, and anyerror![]u8" {
+    {
+        var data = "hi".*;
+        const slice = data[0..];
+        try expect((try peerTypeEmptyArrayAndSliceAndError(true, slice)).len == 0);
+        try expect((try peerTypeEmptyArrayAndSliceAndError(false, slice)).len == 1);
+    }
+    comptime {
+        var data = "hi".*;
+        const slice = data[0..];
+        try expect((try peerTypeEmptyArrayAndSliceAndError(true, slice)).len == 0);
+        try expect((try peerTypeEmptyArrayAndSliceAndError(false, slice)).len == 1);
+    }
+}
+fn peerTypeEmptyArrayAndSliceAndError(a: bool, slice: []u8) anyerror![]u8 {
+    if (a) {
+        return &[_]u8{};
+    }
+    return slice[0..1];
+}
+
+test "peer type resolution: *const T and ?*T" {
+    const a: *const usize = @ptrFromInt(0x123456780);
+    const b: ?*usize = @ptrFromInt(0x123456780);
+    try expect(a == b);
+    try expect(b == a);
+}
