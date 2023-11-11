19c4ab878c324b5f67b71e9ccbaa1bd83435d7b6
zhang-chuanfeng <654300242@qq.com>
2023-11-11 15:37:28 +0800

    [ADD]global assembly

--- assembly.zig
+++ assembly.zig
@@ -29,3 +29,21 @@ pub fn syscall3(number: usize, arg1: usize, arg2: usize, arg3: usize) usize {
         : "rcx", "rll"
     );
 }
+
+const std = @import("std");
+const expect = std.testing.expect;
+
+comptime {
+    asm (
+        \\.global my_func;
+        \\.type my_func, @function;
+        \\my_func:
+        \\ lea (%rdi, %rsi,1),%eax
+        \\retq
+    );
+}
+extern fn my_func(a: i32, b: i32) i32;
+
+test "global assembly" {
+    try expect(my_func(12, 34) == 46);
+}
