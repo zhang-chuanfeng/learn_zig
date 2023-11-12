fb975d24fec35284443ceb483fd0f6eeb6e9d91d
zhang-chuanfeng <654300242@qq.com>
2023-11-12 13:45:28 +0800

    [ADD]test builtin func @This

--- test_builtin.zig
+++ test_builtin.zig
@@ -14,3 +14,21 @@ fn doTheTest() !void {
     try expect(std.mem.eql(u8, src.fn_name, "doTheTest"));
     try expect(std.mem.eql(u8, src.file, "test_builtin.zig"));
 }
+
+//@This()
+
+test "@This()" {
+    var items = [_]i32{ 1, 2, 3, 4 };
+    const list = List(i32){ .items = items[0..] };
+    try expect(list.length() == 4);
+}
+
+fn List(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        items: []T,
+        fn length(self: Self) usize {
+            return self.items.len;
+        }
+    };
+}
