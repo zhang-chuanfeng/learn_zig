af2d5c274096bb8ee982331da1bbf22f6c335a1d
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2023-11-16 18:08:53 +0800

    [MOD]add dynamic lib

--- staticLib/build.zig
+++ staticLib/build.zig
@@ -11,6 +11,14 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const libfizzbuzzdy = b.addSharedLibrary(.{
+        .name = "fizzbuzz",
+        .root_source_file = .{ .path = "fizzbuzz.zig" },
+        .target = target,
+        .optimize = optimize,
+        .version = .{ .major = 1, .minor = 2, .patch = 3 },
+    });
+
     const exe = b.addExecutable(.{
         .name = "demo",
         .root_source_file = .{ .path = "demo.zig" },
@@ -20,6 +28,7 @@ pub fn build(b: *std.Build) void {
     exe.linkLibrary(libfizzbuzz);
 
     b.installArtifact(libfizzbuzz);
+    b.installArtifact(libfizzbuzzdy);
     if (b.option(bool, "enable-demo", "install the demo too") orelse false) {
         b.installArtifact(exe);
     }
