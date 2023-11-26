96eb3b0e8d0ab1ba5398e5a6196a9f549507b36a
zhang-chuanfeng <654300242@qq.com>
2023-11-26 22:57:44 +0800

    [ADD]build example:generate struct

---
+++ generateStruct/src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const Person = @import("person").Person;
+
+pub fn main() !void {
+    const p: Person = .{};
+    try std.io.getStdOut().writer().print("Hello {any}\n", .{p});
+}
