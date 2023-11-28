4c19113967bd20651c3ff08077653d3d7eb89871
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2023-11-28 16:01:16 +0800

    [ADD]build: writeFiles2 generate source file

---
+++ writeFiles2/build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "demo",
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+    b.installArtifact(exe);
+
+    const proto_gen = b.addExecutable(.{
+        .name = "proto_gen",
+        .root_source_file = .{ .path = "tools/proto_gen.zig" },
+    });
+    const run = b.addRunArtifact(proto_gen);
+    const generated_protocol_file = run.addOutputFileArg("protocol.zig");
+
+    const wf = b.addWriteFiles();
+    wf.addCopyFileToSource(generated_protocol_file, "src/protocol.zig");
+
+    const update_protocol_step = b.step("update-protocol", "update src/protocol.zig");
+    update_protocol_step.dependOn(&wf.step);
+}
---
+++ writeFiles2/src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const Protocol = @import("protocol.zig");
+
+pub fn main() !void {
+    const header = try std.io.getStdIn().reader().readStruct(Protocol.Header);
+    std.debug.print("header: {any}\n", .{header});
+}
