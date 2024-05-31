cfa369a8fb14f7d1eef1b9dccdb1bdd89a8f7e57
zhangchuanfeng <zhangchuanfeng@hangzhouyq.com>
2024-05-31 16:30:05 +0800

    [ADD]fizz buzz game

---
+++ fizz_buzz.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var count: u8 = 1;
+    while (count < 100) : (count += 1) {
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+
+        switch (div_3 << 1 | div_5) {
+            0b10 => try stdout.writeAll("Fizz\n"),
+            0b11 => try stdout.writeAll("Fizz Buzz\n"),
+            0b01 => try stdout.writeAll("Buzz\n"),
+            0b00 => try stdout.print("{}\n", .{count}),
+        }
+    }
+}
